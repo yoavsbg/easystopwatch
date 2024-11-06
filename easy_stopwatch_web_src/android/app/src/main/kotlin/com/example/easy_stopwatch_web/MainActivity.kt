b69ffec589dc96028f54bd09d2b91681ae016f69
Yoav Sabag <yoavsbg@users.noreply.github.com>
2024-11-06 13:51:52 +0200

    redesign the landing page (#5)

--- easy_stopwatch_web_src/android/app/src/main/kotlin/com/example/easy_stopwatch_web/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.easy_stopwatch_web
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import LandingPageResponsive from './LandingPageResponsive';
+
+
+function App() {
+
+  return (
+    <>
+      <LandingPageResponsive />
+    </>
+  );
+}
+
+export default App
